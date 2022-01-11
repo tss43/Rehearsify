aa1717b8feed19791982624f38cb8d7594f18720
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-11 22:41:32 +0100

    minor

--- src/misc/df_sorting.py
+++ src/misc/df_sorting.py
@@ -6,6 +6,6 @@ def sort_df( score_df: pd.DataFrame, ignore_str: str ) -> pd.DataFrame:
     """ Sort the score df alphabetically by answer, ignoring the supplied chars. """   
 
     score_df = score_df.sort_values( 
-        by='answer', key=lambda ans: ans.str.replace(pat=f'{ignore_str}', repl='', regex=True) )
+        by='answer', key=lambda ans: ans.str.replace(pat=fr'{ignore_str}', repl='', regex=True) )
 
     return score_df
