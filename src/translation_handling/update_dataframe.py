987042e039c574539cc89af0c381838d53bfd01c
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-13 23:35:03 +0100

    change str.match to ==

--- src/translation_handling/update_dataframe.py
+++ src/translation_handling/update_dataframe.py
@@ -3,18 +3,18 @@
 import pandas as pd
 
 def add_correct_answer(score_df: pd.DataFrame, question: str, further_correct_answer: str) -> pd.DataFrame:
-    """ Add a further correct answer possibility to a translation in the score_df. """
+    """ Add a further correct answer possibility to the translation(s) belonging to the question in the score_df. """
 
-    sample_mask = score_df['question'].str.match(fr'{question}')
+    sample_mask = score_df['question']==question 
     score_df.loc[sample_mask, 'answer'] += '; ' + further_correct_answer
 
     return score_df
 
 
 def decrement_wrong_score(score_df: pd.DataFrame, question: str) -> pd.DataFrame:
-    """ Decrement the wrong score of a translation in the score_df. """
+    """ Decrement the wrong score of the translation(s) belonging to the question in the score_df. """
 
-    sample_mask = score_df['question'].str.match(fr'{question}')
+    sample_mask = score_df['question']==question
     score_df.loc[sample_mask, 'wrong'] -= 1
     score_df.loc[sample_mask, 'wrong_perc'] = 100 * score_df.loc[sample_mask, 'wrong'] / score_df.loc[sample_mask, 'total']
 
