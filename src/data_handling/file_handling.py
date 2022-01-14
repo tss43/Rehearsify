35474667e90e5bbaf798626b7928535ce2d536bd
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-14 21:00:44 +0100

    bugfix in read_dict_txtfile

--- src/data_handling/file_handling.py
+++ src/data_handling/file_handling.py
@@ -17,8 +17,8 @@ def read_dictionary_txtfile(filepath: str) -> pd.DataFrame:
     incomplete_translations = [split_transl for split_transl in translation_list if len(split_transl)!=2 ]
     if incomplete_translations:
         raise ValueError(f"Some translations were incomplete:\n {incomplete_translations}")
-    
-    zero_dict = {COLUMNS[3]: np.NaN, COLUMNS[4]: 0, COLUMNS[5]: 0}
+
+    zero_dict = {COLUMNS[2]: np.NaN, COLUMNS[3]: 0, COLUMNS[4]: 0}
     score_df = pd.DataFrame( translation_list, columns=COLUMNS[:2] ).assign(**zero_dict)
 
     return score_df
