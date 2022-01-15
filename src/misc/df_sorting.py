30083f6d240963504c3e6b6f1f8b78a4abc6f89d
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-15 10:25:47 +0100

    expanded sort key in sort_df

--- src/misc/df_sorting.py
+++ src/misc/df_sorting.py
@@ -2,10 +2,22 @@
 
 import pandas as pd
 
-def sort_df( score_df: pd.DataFrame, ignore_str: str ) -> pd.DataFrame:
-    """ Sort the score df alphabetically by answer, ignoring the supplied chars. """   
+import string
+import unidecode 
 
-    score_df = score_df.sort_values( 
-        by='answer', key=lambda ans: ans.str.replace(pat=fr'{ignore_str}', repl='', regex=True) )
+def sort_df(score_df: pd.DataFrame, ignore_str: str) -> pd.DataFrame:
+    """ Sort the score df alphabetically by answer, ignoring the supplied characters. """   
+
+    def reduce_series(ans: pd.Series) -> pd.Series:
+        """ Reduce a Series by ignoring the supplied characters and special characters/punctuation/case """
+        
+        ans = ans.str.replace( pat=fr'{ignore_str}', repl='', regex=True )
+        ans = ans.apply( unidecode.unidecode )
+        ans = ans.str.translate( str.maketrans('', '', string.punctuation) )
+        ans = ans.str.lower()
+
+        return ans
+
+    score_df = score_df.sort_values( by='answer', key=reduce_series )
 
     return score_df
