5c78a19475c948d1fc67daa11423bf85f0e4f847
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-20 23:00:00 +0100

    defining COLUMN_DTYPES and DISPLAY_COLUMNS

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,6 @@
 """ Defines constants for other modules to import. """
 
 COLUMNS = ['question', 'answer', 'wrong_perc', 'wrong', 'total']
+COLUMN_DTYPES = ['O','O','float64', 'int64', 'int64'] 
+
+DISPLAY_COLUMNS = ['X/O', 'Question', 'Correct answer', 'User answer', 'Wrong/total']
