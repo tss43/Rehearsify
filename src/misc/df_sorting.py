cdc3bb0bbf4e4615b1f3baf4593092b6eba55511
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-27 22:16:01 +0100

    comment no resetting index in sort_df

--- src/misc/df_sorting.py
+++ src/misc/df_sorting.py
@@ -5,10 +5,10 @@ import pandas as pd
 import unidecode 
 
 def sort_df(score_df: pd.DataFrame, ignore_str: str) -> pd.DataFrame:
-    """ Sort the score df alphabetically by answer, ignoring the supplied characters. """   
+    """ Sort the score df alphabetically by answer, ignoring the supplied characters. Do not reset index. """   
 
     def reduce_series(ans: pd.Series) -> pd.Series:
-        """ Reduce a Series by ignoring the supplied characters and special characters and case """
+        """ Reduce a Series by ignoring the supplied characters and special characters and case. """
         
         ans = ans.str.replace( pat=fr'{ignore_str}', repl='', regex=True )
         ans = ans.apply( unidecode.unidecode )
