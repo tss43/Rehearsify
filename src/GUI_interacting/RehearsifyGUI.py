e8f9d8e58dd43881a6c170093f9c504756415dd7
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-01-29 10:09:30 +0100

    fixed spelling mistake

--- src/GUI_interacting/RehearsifyGUI.py
+++ src/GUI_interacting/RehearsifyGUI.py
@@ -13,7 +13,7 @@ import pandas as pd
 from src.data_handling.file_handling import (
     read_dictionary_txtfile, update_with_df, save_as_dictionary_txtfile, 
     validate_translation_dictionary, validate_ignore_str )
-from src.translation_handling.question_selecting import select_randomly_weighed_question
+from src.translation_handling.question_selecting import select_randomly_weighted_question
 from src.translation_handling.answer_handling import check_answer
 from src.translation_handling.update_dataframe import add_correct_answer, update_sample_score, decrement_sample_wrong_score
 
@@ -285,7 +285,7 @@ class RehearsifyGUI:
 
 
         # select new question and update prompt
-        self.sample = select_randomly_weighed_question( self.score_df )
+        self.sample = select_randomly_weighted_question( self.score_df )
         self.question.set(self.sample.question)
 
 
--- src/translation_handling/question_selecting.py
+++ src/translation_handling/question_selecting.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-def select_randomly_weighed_question( score_df: pd.DataFrame, seed=None ) -> pd.Series:
+def select_randomly_weighted_question( score_df: pd.DataFrame, seed=None ) -> pd.Series:
     """ select a randomly selected word from the score df, weighted by percentage wrong if possible """
 
-    # take a random sample out of all rows, weighed by the percentage wrong. Replacing NaNs -> 10% for unpractised translations,
+    # take a random sample out of all rows, weighted by the percentage wrong. Replacing NaNs -> 10% for unpractised translations,
     # and clipping to to minimally 1% to give some non-zero probability to translations previously correctly answered. 
     clipped_weights = score_df['wrong_perc'].fillna( value=10.0, inplace=False ).clip(lower=1.0)
     sample = score_df.sample( n=1, weights=clipped_weights, random_state=seed ).squeeze() 
