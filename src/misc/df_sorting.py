4cdb6375b0d657d197f153c6ca9baf843a4c88fa
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-02-12 20:23:38 +0100

    bugfix sortby_str_df

--- src/misc/df_sorting.py
+++ src/misc/df_sorting.py
@@ -26,7 +26,7 @@ def sortby_num_df( df: pd.DataFrame, sortby: str ) -> pd.DataFrame:
 def sortby_str_df(df: pd.DataFrame, sortby: str, ignore_str: str='') -> pd.DataFrame:
     """ Sort the DataFrame alphabetically by a string col named sortby, ignoring the supplied characters. Do not reset index. """   
 
-    def reduce_series(ans: pd.Series[str], ignore_str: str) -> pd.Series[str]:
+    def reduce_series(ans: pd.Series, ignore_str: str) -> pd.Series:
         """ Reduce a string Series by ignoring the supplied characters and special characters and case. """
         
         ans = ans.str.replace( pat=fr'{ignore_str}', repl='', regex=True )
@@ -35,6 +35,6 @@ def sortby_str_df(df: pd.DataFrame, sortby: str, ignore_str: str='') -> pd.DataF
 
         return ans
 
-    df = df.sort_values( by=sortby, key=lambda str=ignore_str: reduce_series(str) )
+    df = df.sort_values( by=sortby, key=lambda ans, str=ignore_str: reduce_series(ans=ans, ignore_str=str) )
 
     return df
