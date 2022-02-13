dbba3f6a1201b74c4e740f158e0727c3f034c088
Tycho Sikkenk <tycho.sikkenk@gmail.com>
2022-02-13 13:35:43 +0100

    implemented test sorting functions

--- tests/misc/test_df_sorting.py
+++ tests/misc/test_df_sorting.py
@@ -3,10 +3,11 @@
 import unittest
 from pandas.testing import assert_frame_equal      # for testing dataframes
 
+import numpy as np
 import pandas as pd
 from itertools import product as cartprod
 
-from src.misc.df_sorting import sortby_str_df
+from src.misc.df_sorting import no_sort_df, random_sort_df, sortby_num_df, sortby_str_df
 
 from src.constants import COLUMNS
 
@@ -14,70 +15,146 @@ class TestDfSorting(unittest.TestCase):
     """ Tests of the sorting of the score df. """
     
     def setUp(self):
-        """ Initialise attributes available for tests in class. """
-        
+        """ Hook method for setting up the test fixture before exercising it. """
         self.score_df_list = [
             pd.DataFrame(
                 data=[ 
-                    ['b', 'q0', 0, 0, 0],
-                    ['a', 'q1', 0, 0, 0],
-                    ['c', 'q2', 0, 0, 0] ],
+                    ['b', 'q0', np.NaN, 0, 0],
+                    ['a', 'q1', 100., 1, 1],
+                    ['c', 'q2', 50., 2, 4] ],
                 columns=COLUMNS ),
             pd.DataFrame( 
                 data=[
-                    ['the (non)sense', 'q0', 0., 0, 0],
-                    ['an ant', 'q1', 0., 0, 0],
-                    ['the aardvark', 'q2', 0., 0, 0],
-                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 0., 0, 0] ],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['an ant', 'q1', 20., 1, 5],
+                    ['the aardvark', 'q2', 30., 3, 10],
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10] ],
                 columns=COLUMNS ) ]
-
-        self.ignore_str_list = [ '', 'a |the | an' ]
 
 
     def tearDown(self):
-        """ Deinitialise attributes available for tests in class. """
-
+        """ Hook method for deconstructing the test fixture after testing it. """
         self.score_df_list = None
-        self.ignore_str_list = None
+
+    def test_no_sort_df(self):
+        """ Testing not sorting a dataframe at all. """
+
+        expected_not_sorted_score_df_list = self.score_df_list
+
+        for score_df, expected_not_sorted_score_df in zip(self.score_df_list, expected_not_sorted_score_df_list):
+            not_sorted_score_df = no_sort_df(score_df)
+            assert_frame_equal( not_sorted_score_df, expected_not_sorted_score_df )   
 
 
-    def sortby_str_df(self):
-        """ Testing the sorting of a dataframe. """
+    def test_random_sort_df(self):
+        """ Testing the random sorting of a dataframe. """
+
+        seed = 1
 
         expected_sorted_score_df_list = [
             pd.DataFrame(
                 data=[ 
-                    ['a', 'q1', 0, 0, 0],
-                    ['b', 'q0', 0, 0, 0],
-                    ['c', 'q2', 0, 0, 0] ],
+                    ['b', 'q0', np.NaN, 0, 0],
+                    ['c', 'q2', 50., 2, 4],
+                    ['a', 'q1', 100., 1, 1] ],
+                columns=COLUMNS ),
+            pd.DataFrame( 
+                data=[
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10],
+                    ['the aardvark', 'q2', 30., 3, 10],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['an ant', 'q1', 20., 1, 5] ],
+                columns=COLUMNS ) ]
+
+        for score_df, expected_sorted_score_df in zip(self.score_df_list, expected_sorted_score_df_list):
+
+            sorted_score_df = random_sort_df(df=score_df, seed=seed)
+            
+            with self.subTest(f"{sorted_score_df} -> {expected_sorted_score_df}"): 
+                assert_frame_equal( sorted_score_df.reset_index(drop=True), expected_sorted_score_df )
+
+
+    def test_sortby_num_df(self):
+        """ Testing the sorting of a dataframe on a numerical column. """
+
+        sortby_list = ['wrong_perc', 'total']
+
+        expected_sorted_score_df_list = [
+            pd.DataFrame(
+                data=[ 
+                    ['c', 'q2', 50., 2, 4],
+                    ['a', 'q1', 100., 1, 1],
+                    ['b', 'q0', np.NaN, 0, 0] ],
                 columns=COLUMNS ),
             pd.DataFrame(
                 data=[ 
-                    ['a', 'q1', 0, 0, 0],
-                    ['b', 'q0', 0, 0, 0],
-                    ['c', 'q2', 0, 0, 0] ],
+                    ['b', 'q0', np.NaN, 0, 0],
+                    ['a', 'q1', 100., 1, 1],
+                    ['c', 'q2', 50., 2, 4] ],
                 columns=COLUMNS ),
             pd.DataFrame( 
                 data=[
-                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 0., 0, 0],
-                    ['an ant', 'q1', 0., 0, 0],
-                    ['the (non)sense', 'q0', 0., 0, 0],
-                    ['the aardvark', 'q2', 0., 0, 0] ],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['an ant', 'q1', 20., 1, 5],
+                    ['the aardvark', 'q2', 30., 3, 10],
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10] ],
                 columns=COLUMNS ),
-            pd.DataFrame( 
+                pd.DataFrame( 
                 data=[
-                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 0., 0, 0],
-                    ['the (non)sense', 'q0', 0., 0, 0],
-                    ['the aardvark', 'q2', 0., 0, 0],
-                    ['an ant', 'q1', 0., 0, 0] ],
+                    ['an ant', 'q1', 20., 1, 5],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['the aardvark', 'q2', 30., 3, 10],
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10] ],
                 columns=COLUMNS ) ]
 
-        for (score_df, ignore_str), expected_sorted_score_df in zip(cartprod(
-            self.score_df_list, self.ignore_str_list), expected_sorted_score_df_list):
+        for (score_df, sortby), expected_sorted_score_df in zip(
+            cartprod(self.score_df_list, sortby_list), expected_sorted_score_df_list):
+
+            sorted_score_df = sortby_num_df(df=score_df, sortby=sortby)
             
-            sorted_score_df = sortby_str_df(score_df, ignore_str)
+            with self.subTest(f"{sorted_score_df} -> {expected_sorted_score_df}"): 
+                assert_frame_equal( sorted_score_df.reset_index(drop=True), expected_sorted_score_df )
+
+
+    def test_sortby_str_df(self):
+        """ Testing the sorting of a dataframe on a string column. """
+
+        answer_ignore_str_list = [ '', 'a |the | an' ]
+
+        expected_sorted_score_df_list = [
+            pd.DataFrame(
+                data=[ 
+                    ['a', 'q1', 100., 1, 1],
+                    ['b', 'q0', np.NaN, 0, 0],
+                    ['c', 'q2', 50., 2, 4] ],
+                columns=COLUMNS ),
+            pd.DataFrame(
+                data=[ 
+                    ['a', 'q1', 100., 1, 1],
+                    ['b', 'q0', np.NaN, 0, 0],
+                    ['c', 'q2', 50., 2, 4] ],
+                columns=COLUMNS ),
+            pd.DataFrame( 
+                data=[
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10],
+                    ['an ant', 'q1', 20., 1, 5],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['the aardvark', 'q2', 30., 3, 10] ],
+                columns=COLUMNS ),
+            pd.DataFrame( 
+                data=[
+                    ['\'a zebra\' doesn\'t lose its stripes', 'q3', 40., 4, 10],
+                    ['the (non)sense', 'q0', 10., 1, 10],
+                    ['the aardvark', 'q2', 30., 3, 10],
+                    ['an ant', 'q1', 20., 1, 5] ],
+                columns=COLUMNS ) ]
+
+        for (score_df, answer_ignore_str), expected_sorted_score_df in zip(
+            cartprod(self.score_df_list, answer_ignore_str_list), expected_sorted_score_df_list):
+
+            sorted_score_df = sortby_str_df(df=score_df, sortby='answer', ignore_str=answer_ignore_str)
             
-            with self.subTest(f"{sorted_score_df} with ignore_str {ignore_str} -> {expected_sorted_score_df}"): 
+            with self.subTest(f"{sorted_score_df} with ignore_str {answer_ignore_str} -> {expected_sorted_score_df}"): 
                 assert_frame_equal( sorted_score_df.reset_index(drop=True), expected_sorted_score_df )
 
             
